0cee8ef5405f04d8fb820c424eb3c88c2e1e5ee9
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2019-04-05 18:34:07 +0100

    initial start on re-finding missing genes

--- squeaky/squeaky.py
+++ squeaky/squeaky.py
@@ -3,6 +3,7 @@ from cdhit import run_cdhit
 from generate_network import generate_network
 from generate_output import *
 from clean_network import *
+from find_missing import find_missing
 import os
 import argparse
 import tempfile
@@ -117,16 +118,16 @@ def main():
     temp_dir = tempfile.mkdtemp(dir=args.output_dir)
 
     # convert input GFF3 files into summary files
-    process_prokka_input(args.input_files, args.output_dir)
+    # process_prokka_input(args.input_files, args.output_dir)
 
     # Cluster protein sequences using cdhit
     cd_hit_out = args.output_dir + "combined_protein_cdhit_out.txt"
-    run_cdhit(
-        input_file=args.output_dir + "combined_protein_CDS.fasta",
-        output_file=cd_hit_out,
-        id=args.id,
-        s=args.len_dif_percent,
-        n_cpu=args.n_cpu)
+    # run_cdhit(
+    #     input_file=args.output_dir + "combined_protein_CDS.fasta",
+    #     output_file=cd_hit_out,
+    #     id=args.id,
+    #     s=args.len_dif_percent,
+    #     n_cpu=args.n_cpu)
 
     # generate network from clusters and adjacency information
     G = generate_network(
@@ -135,40 +136,43 @@ def main():
         prot_seq_file=args.output_dir + "combined_protein_CDS.fasta",
         split_paralogs=args.split_paralogs)
 
-    # # write out pre-filter graph in GML format
-    # nx.write_gml(G, args.output_dir + "pre_filt_graph.gml")
-    #
-    # # remove low support trailing ends
-    # G = trim_low_support_trailing_ends(
-    #     G,
-    #     min_support=args.min_trailing_support,
-    #     max_recursive=args.trailing_recursive)
-    #
-    # # clean up translation errors and gene families
-    # G = collapse_families(
-    #     G,
-    #     cycle_threshold=args.max_cycle_size,
-    #     family_threshold=args.family_threshold,
-    #     outdir=temp_dir,
-    #     dna_error_threshold=0.99,
-    #     correct_mistranslations=True)
-    #
-    # # add helpful attributes and write out graph in GML format
-    # for node in G.nodes():
-    #     G.node[node]['genomeIDs'] = ";".join(G.node[node]['members'])
-    #     G.node[node]['degrees'] = G.degree[node]
-    # nx.write_gml(G, args.output_dir + "final_graph.gml")
-    #
-    # # write out roary like gene_presence_absence.csv
-    # generate_roary_gene_presence_absence(
-    #     G,
-    #     file_names=args.input_files,
-    #     dna_file=args.output_dir + "combined_DNA_CDS.fasta",
-    #     output_dir=args.output_dir)
-    #
-    # # write pan genome reference fasta file
-    # generate_pan_genome_reference(
-    #     G, output_dir=args.output_dir, split_paralogs=False)
+    # write out pre-filter graph in GML format
+    nx.write_gml(G, args.output_dir + "pre_filt_graph.gml")
+
+    # remove low support trailing ends
+    G = trim_low_support_trailing_ends(
+        G,
+        min_support=args.min_trailing_support,
+        max_recursive=args.trailing_recursive)
+
+    # clean up translation errors and gene families
+    G = collapse_families(
+        G,
+        cycle_threshold=args.max_cycle_size,
+        family_threshold=args.family_threshold,
+        outdir=temp_dir,
+        dna_error_threshold=0.99,
+        correct_mistranslations=True)
+
+    # find genes that Prokka has missed
+    find_missing(G, args.input_files)
+
+    # add helpful attributes and write out graph in GML format
+    for node in G.nodes():
+        G.node[node]['genomeIDs'] = ";".join(G.node[node]['members'])
+        G.node[node]['degrees'] = G.degree[node]
+    nx.write_gml(G, args.output_dir + "final_graph.gml")
+
+    # write out roary like gene_presence_absence.csv
+    generate_roary_gene_presence_absence(
+        G,
+        file_names=args.input_files,
+        dna_file=args.output_dir + "combined_DNA_CDS.fasta",
+        output_dir=args.output_dir)
+
+    # write pan genome reference fasta file
+    generate_pan_genome_reference(
+        G, output_dir=args.output_dir, split_paralogs=False)
 
     # remove temp TemporaryDirectory
     shutil.rmtree(temp_dir)
