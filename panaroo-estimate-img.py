2812214c60c3b1eb06a259fe45c0c3ff9499b06e
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2019-09-24 17:23:00 +0200

    re-added renamed scripts

---
+++ panaroo-estimate-img.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""Wrapper for running the panaroo IMG model esimtator on gene frequencies"""
+
+from panaroo.estimate_img_params import main
+
+if __name__ == '__main__':
+    main()
---
+++ post-plot-runner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""Wrapper for running plotting functions directly from source tree."""
+
+from panaroo.generate_abundance_plots import main
+
+if __name__ == '__main__':
+    main()
