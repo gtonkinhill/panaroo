4500e5f7995a49ecb5656dd57a22204340e9c578
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2019-10-17 12:52:07 +0000

    updated COG script to use DIAMOND instead of BLAST

--- scripts/simulate_reads_assem_prok.sh
+++ scripts/simulate_reads_assem_prok.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 set -e
 
+#conda activate panaroo
+
 fasta=$1
 nreads=$2
 ncpus=$3
 
-full_fasta=$(realpath $fasta)
+full_fasta=$(readlink -f $fasta)
 prefix=$(basename $fasta .fasta)
+
+cd ngs_art_sim
 
 mkdir $prefix
 
 # simulate reads
 cd $prefix
 # mason_simulator --num-threads $ncpus --seq-technology illumina -ir $full_fasta -n $nreads -o "${prefix}_left.fq" -or "${prefix}_right.fq" --seed $RANDOM
-art_illumina -ss HS25 -na -i $full_fasta -p -l 150 -f 20 -m 200 -s 10 -o $prefix
+# art_illumina -ss HS25 -na -i $full_fasta -p -l 150 -f 20 -m 200 -s 10 -o $prefix
+art_illumina -ss HS25 -i $full_fasta -o ${prefix}_ -l 150 -f 20 -p -m 200 -s 10 --noALN
 
 # assemble
-spades.py --threads $ncpus -1 "${prefix}1.fq" -2 "${prefix}2.fq" -o ./
+spades.py --phred-offset 33 --threads $ncpus -1 "${prefix}_1.fq" -2 "${prefix}_2.fq" -o ./
 
 # clean up reads
 rm *.fq
@@ -33,8 +38,8 @@ rm dataset.info
 rm input_dataset.yaml
 rm scaffolds.paths
 
-# run prokka
-# prokka_prefix="prokka_${prefix}"
-# prokka --noanno --cpus $ncpus --outdir $prokka_prefix --prefix $prokka_prefix scaffolds.fasta
+cd ../..
 
-cd ..
+
+# run prokka
+prokka --proteins pan_sim_*prokka_DB.fasta --cpus $ncpus --outdir ./prokka_art_assem/${prefix} --prefix $prefix ./ngs_art_sim/${prefix}/scaffolds.fasta
