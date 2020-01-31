09149daae200dd1c9dd7f37eb403439faa8096b0
nm12 <nm12@sanger.ac.uk>
2020-01-31 19:01:02 +0000

    Bugfix for aligning sequences with only 1 node

--- panaroo/generate_alignments.py
+++ panaroo/generate_alignments.py
@@ -17,6 +17,8 @@ def output_sequence(node, isolate_list, temp_directory, outdir):
     #Get the name of the sequences for the gene of interest
     sequence_ids = node["seqIDs"]
     output_sequences = []
+    #Counter for the number of sequences to
+    isolate_no = 0
     #Look for gene sequences among all genes (from disk)
     for seq in SeqIO.parse(outdir + "combined_DNA_CDS.fasta", 'fasta'):
         isolate_num = int(seq.id.split('_')[0])
@@ -25,10 +27,17 @@ def output_sequence(node, isolate_list, temp_directory, outdir):
         if seq.id in sequence_ids:
             output_sequences.append(
                 SeqRecord(seq.seq, id=isolate_name, description=""))
+            isolate_no += 1
     #Put gene of interest sequences in a generator, with corrected isolate names
     output_sequences = (x for x in output_sequences)
-    #set filename to gene name
-    outname = temp_directory + node["name"] + ".fasta"
+    #set filename to gene name, if more than one sequence to be aliged
+    if isolate_no > 1:
+        outname = temp_directory + node["name"] + ".fasta"
+    else:
+        #If only one sequence, output it to aliged directory and break
+        outname = "./aligned_gene_sequences" + node["name"] + ".fasta"
+        SeqIO.write(output_sequences, outname, 'fasta')
+        return None
     #check to see if filename is too long
     if len(outname) >= 248:
         outname = outname[:248] + ".fasta"
