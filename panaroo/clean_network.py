6b548c7a0713ed5916ba7dc91f4d9468f2330303
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2020-02-05 12:37:01 +0000

    fixed issue with merging paralogs option

--- panaroo/clean_network.py
+++ panaroo/clean_network.py
@@ -11,6 +11,7 @@ from scipy.sparse.csgraph import connected_components, shortest_path
 from tqdm import tqdm
 import sys
 
+
 # Genes at the end of contigs are more likely to be false positives thus
 # we can remove those with low support
 def trim_low_support_trailing_ends(G, min_support=3, max_recursive=2):
@@ -30,6 +31,7 @@ def trim_low_support_trailing_ends(G, min_support=3, max_recursive=2):
         if not removed: break
 
     return G
+
 
 def mod_bfs_edges(G, source, depth_limit=None):
     """Iterate over edges in a breadth-first search.
@@ -53,6 +55,7 @@ def mod_bfs_edges(G, source, depth_limit=None):
         except StopIteration:
             queue.popleft()
 
+
 def single_linkage(G, distances_bwtn_centroids, centroid_to_index, neighbours):
     index = []
     neigh_array = []
@@ -70,15 +73,16 @@ def single_linkage(G, distances_bwtn_centroids, centroid_to_index, neighbours):
     # labels = labels[index]
     for neigh in neighbours:
         l = list(set(labels[neigh_array == neigh]))
-        if len(l)>1:
+        if len(l) > 1:
             for i in l[1:]:
-                labels[labels==i] = l[0]
+                labels[labels == i] = l[0]
 
     clusters = [
         del_dups(list(neigh_array[labels == i])) for i in np.unique(labels)
     ]
 
-    return(clusters)
+    return (clusters)
+
 
 def collapse_families(G,
                       seqid_to_centroid,
@@ -88,12 +92,11 @@ def collapse_families(G,
                       correct_mistranslations=False,
                       n_cpu=1,
                       quiet=False,
-                      distances_bwtn_centroids=None, 
+                      distances_bwtn_centroids=None,
                       centroid_to_index=None):
 
     node_count = max(list(G.nodes())) + 10
 
-    
     if correct_mistranslations:
         depths = [1, 2, 3]
         threshold = [0.99, 0.98, 0.95, 0.9]
@@ -103,15 +106,14 @@ def collapse_families(G,
 
     # precluster for speed
     if correct_mistranslations:
-        cdhit_clusters = iterative_cdhit(
-            G,
-            outdir,
-            thresholds=threshold,
-            n_cpu=n_cpu,
-            quiet=True,
-            dna=True,
-            word_length=7,
-            accurate=False)
+        cdhit_clusters = iterative_cdhit(G,
+                                         outdir,
+                                         thresholds=threshold,
+                                         n_cpu=n_cpu,
+                                         quiet=True,
+                                         dna=True,
+                                         word_length=7,
+                                         accurate=False)
         distances_bwtn_centroids, centroid_to_index = pwdist_edlib(
             G, cdhit_clusters, dna_error_threshold, dna=True, n_cpu=n_cpu)
     elif distances_bwtn_centroids is None:
@@ -129,13 +131,15 @@ def collapse_families(G,
     for node in G.nodes():
         for sid in G.nodes[node]['seqIDs']:
             if "refound" in sid:
-                seqid_to_index[sid] = centroid_to_index[G.nodes[node]["longCentroidID"][1]]
+                seqid_to_index[sid] = centroid_to_index[G.nodes[node]
+                                                        ["longCentroidID"][1]]
             else:
                 seqid_to_index[sid] = centroid_to_index[seqid_to_centroid[sid]]
 
     nonzero_dist = distances_bwtn_centroids.nonzero()
-    nonzero_dist = set([(i,j) for i,j in zip(nonzero_dist[0], nonzero_dist[1])])
-    
+    nonzero_dist = set([(i, j)
+                        for i, j in zip(nonzero_dist[0], nonzero_dist[1])])
+
     for depth in depths:
         search_space = set(G.nodes())
         while len(search_space) > 0:
@@ -152,11 +156,13 @@ def collapse_families(G,
 
                 # find neighbouring nodes and cluster their centroid with cdhit
                 neighbours = [
-                    v for u, v in nx.bfs_edges(G, source=node, depth_limit=depth)
-                ] + [node] 
+                    v
+                    for u, v in nx.bfs_edges(G, source=node, depth_limit=depth)
+                ] + [node]
 
                 # find clusters
-                clusters =  single_linkage(G, distances_bwtn_centroids, centroid_to_index, neighbours)
+                clusters = single_linkage(G, distances_bwtn_centroids,
+                                          centroid_to_index, neighbours)
 
                 for cluster in clusters:
 
@@ -193,19 +199,23 @@ def collapse_families(G,
                         search_space.add(node_count)
                     else:
                         # merge if the centroids don't conflict and the nodes are adjacent in the conflicting genome
-                        # this corresponds to a mistranslation/frame shift/premature stop where one gene has been split 
+                        # this corresponds to a mistranslation/frame shift/premature stop where one gene has been split
                         # into two in a subset of genomes
-                        
+
                         # build a mini graph of allowed pairwise merges
                         tempG = nx.Graph()
                         for nA, nB in itertools.combinations(cluster, 2):
-                            mem_inter = G.nodes[nA]['members'].intersection(G.nodes[nB]['members'])
+                            mem_inter = G.nodes[nA]['members'].intersection(
+                                G.nodes[nB]['members'])
                             if len(mem_inter) > 0:
-                                
+
                                 shouldmerge = True
-                                if len(set(G.nodes[nA]['centroid']).intersection(set(G.nodes[nB]['centroid'])))>0:
-                                        shouldmerge=False
-                                        
+                                if len(
+                                        set(G.nodes[nA]['centroid']).
+                                        intersection(
+                                            set(G.nodes[nB]['centroid']))) > 0:
+                                    shouldmerge = False
+
                                 if shouldmerge:
                                     idsA = defaultdict(list)
                                     for sid in G.nodes[nA]['seqIDs']:
@@ -220,59 +230,75 @@ def collapse_families(G,
                                             idsB[ssid[0]].append(sid)
 
                                     for imem in mem_inter:
-                                        for sidA in set([seqid_to_index[sid] for sid in idsA[imem]]):
-                                            for sidB in set([seqid_to_index[sid] for sid in idsB[imem]]):
-                                                    if ((sidA, sidB) in nonzero_dist) or ((sidB, sidA) in nonzero_dist):
-                                                        shouldmerge=False
-                                                        break
+                                        for sidA in set([
+                                                seqid_to_index[sid]
+                                                for sid in idsA[imem]
+                                        ]):
+                                            for sidB in set([
+                                                    seqid_to_index[sid]
+                                                    for sid in idsB[imem]
+                                            ]):
+                                                if (
+                                                    (sidA,
+                                                     sidB) in nonzero_dist
+                                                ) or ((sidB,
+                                                       sidA) in nonzero_dist):
+                                                    shouldmerge = False
+                                                    break
                                             if not shouldmerge: break
                                         if not shouldmerge: break
- 
+
                                 if shouldmerge:
                                     tempG.add_edge(nA, nB)
                             else:
                                 tempG.add_edge(nA, nB)
 
                         # merge from largest clique to smallest
-                        sys.setrecursionlimit(max(len(tempG.nodes),10000))
+                        sys.setrecursionlimit(max(len(tempG.nodes), 10000))
                         clique = max_clique(tempG)
-                        while len(clique)>1:
-                            clique_clusters = single_linkage(G, distances_bwtn_centroids, centroid_to_index, clique)
+                        while len(clique) > 1:
+                            clique_clusters = single_linkage(
+                                G, distances_bwtn_centroids, centroid_to_index,
+                                clique)
                             for clust in clique_clusters:
-                                if len(clust)<=1: continue
+                                if len(clust) <= 1: continue
                                 node_count += 1
                                 for neig in clust:
                                     removed_nodes.add(neig)
                                     if neig in search_space:
                                         search_space.remove(neig)
-                                
+
                                 temp_c = clust.copy()
-                                G = merge_nodes(G,
-                                                temp_c.pop(),
-                                                temp_c.pop(),
-                                                node_count,
-                                                multi_centroid=(not correct_mistranslations),
-                                                check_merge_mems=False)
+                                G = merge_nodes(
+                                    G,
+                                    temp_c.pop(),
+                                    temp_c.pop(),
+                                    node_count,
+                                    multi_centroid=(
+                                        not correct_mistranslations),
+                                    check_merge_mems=False)
                                 while (len(temp_c) > 0):
-                                    G = merge_nodes(G,
-                                                    node_count,
-                                                    temp_c.pop(),
-                                                    node_count + 1,
-                                                    multi_centroid=(not correct_mistranslations),
-                                                    check_merge_mems=False)
+                                    G = merge_nodes(
+                                        G,
+                                        node_count,
+                                        temp_c.pop(),
+                                        node_count + 1,
+                                        multi_centroid=(
+                                            not correct_mistranslations),
+                                        check_merge_mems=False)
                                     node_count += 1
                                 search_space.add(node_count)
                             tempG.remove_nodes_from(clique)
                             clique = max_clique(tempG)
-                
+
                 if node in search_space:
                     search_space.remove(node)
 
     return G, distances_bwtn_centroids, centroid_to_index
 
 
-def collapse_paralogs(G, centroid_contexts, max_context=5, quiet=False): 
-    
+def collapse_paralogs(G, centroid_contexts, max_context=5, quiet=False):
+
     node_count = max(list(G.nodes())) + 10
 
     # first sort by context length, context dist to ensure ties
@@ -282,7 +308,7 @@ def collapse_paralogs(G, centroid_contexts, max_context=5, quiet=False):
 
     # set up for context search
     centroid_to_index = {}
-    ncentroids=-1
+    ncentroids = -1
     for node in G.nodes():
         centroid = G.nodes[node]['centroid'][0]
         if centroid not in centroid_to_index:
@@ -290,7 +316,8 @@ def collapse_paralogs(G, centroid_contexts, max_context=5, quiet=False):
             centroid_to_index[centroid] = ncentroids
             centroid_to_index[G.nodes[node]['centroid'][0]] = ncentroids
         else:
-            centroid_to_index[G.nodes[node]['centroid'][0]] = centroid_to_index[centroid]
+            centroid_to_index[G.nodes[node]['centroid']
+                              [0]] = centroid_to_index[centroid]
     ncentroids += 1
 
     for centroid in tqdm(centroid_contexts):
@@ -312,11 +339,11 @@ def collapse_paralogs(G, centroid_contexts, max_context=5, quiet=False):
             s_max = -np.inf
             best_cluster = None
 
-            if para[1]==ref_paralogs[0][1]:
+            if para[1] == ref_paralogs[0][1]:
                 # this is the reference so skip
                 continue
 
-            # first attempt by shortest path            
+            # first attempt by shortest path
             for c, ref in enumerate(ref_paralogs):
                 if para[1] in cluster_mems[c]:
                     #dont match paralogs of the same isolate
@@ -327,62 +354,93 @@ def collapse_paralogs(G, centroid_contexts, max_context=5, quiet=False):
                     d = nx.shortest_path_length(G, ref[0], para[0])
                 except nx.NetworkXNoPath:
                     continue
-                if d<d_max:
+                if d < d_max:
                     d_max = d
                     best_cluster = c
 
             # if this fails use context
-            if d_max==np.inf:
+            if d_max == np.inf:
                 best_cluster = 0
                 s_max = -np.inf
                 para_context = np.zeros(ncentroids)
                 for u, node, depth in mod_bfs_edges(G, para[0], max_context):
-                    para_context[centroid_to_index[G.nodes[node]['centroid'][0]]] = depth
+                    para_context[centroid_to_index[G.nodes[node]['centroid']
+                                                   [0]]] = depth
                 for c, ref in enumerate(ref_paralogs):
                     if para[1] in cluster_mems[c]:
                         #dont match paralogs of the same isolate
                         continue
                     ref_context = np.zeros(ncentroids)
-                    for u, node, depth in mod_bfs_edges(G, ref[0], max_context):
-                        ref_context[centroid_to_index[G.nodes[node]['centroid'][0]]] = depth
-                    s = np.sum(1/(1+np.abs((para_context - ref_context)[(para_context*ref_context)!=0])))
-                    if s>s_max:
-                        s_max=s
+                    for u, node, depth in mod_bfs_edges(
+                            G, ref[0], max_context):
+                        ref_context[centroid_to_index[G.nodes[node]['centroid']
+                                                      [0]]] = depth
+                    s = np.sum(1 / (1 + np.abs((para_context - ref_context)[
+                        (para_context * ref_context) != 0])))
+                    if s > s_max:
+                        s_max = s
                         best_cluster = c
-            
+
             cluster_dict[best_cluster].add(para[0])
             cluster_mems[best_cluster].add(para[1])
-        
+
         # merge
         for cluster in cluster_dict:
-            if len(cluster_dict[cluster])<2: continue
+            if len(cluster_dict[cluster]) < 2: continue
             temp_c = list(cluster_dict[cluster].copy())
             node_count += 1
-            G = merge_nodes(G, temp_c.pop(), temp_c.pop(), node_count)            
+            G = merge_nodes(G, temp_c.pop(), temp_c.pop(), node_count)
             while (len(temp_c) > 0):
-                G = merge_nodes(G, node_count, temp_c.pop(), node_count + 1)    
+                G = merge_nodes(G, node_count, temp_c.pop(), node_count + 1)
                 node_count += 1
-                
 
-    return(G)
+    return (G)
+
 
 def merge_paralogs(G):
 
     node_count = max(list(G.nodes())) + 10
 
     # group paralog nodes by centroid
-    paralog_centroid_dict = defaultdict(list)
+    paralog_centroids = defaultdict(list)
     for node in G.nodes():
         if G.nodes[node]['paralog']:
-            paralog_centroid_dict[G.nodes[node]['centroid'][0]].append(node)
+            for centroid in G.nodes[node]['centroid']:
+                paralog_centroids[centroid].append(node)
+
+    # find nodes that share common centroids
+    paralog_centroids = paralog_centroids.values()
+    merge_clusters = []
+    while len(paralog_centroids) > 0:
+        first, *rest = paralog_centroids
+        first = set(first)
+        lf = -1
+        while len(first) > lf:
+            lf = len(first)
+            rest2 = []
+            for r in rest:
+                if len(first.intersection(set(r))) > 0:
+                    first |= set(r)
+                else:
+                    rest2.append(r)
+            rest = rest2
+        merge_clusters.append(first)
+        paralog_centroids = rest
 
     # merge paralog nodes that share the same centroid
-    for centroid in paralog_centroid_dict:
+    for temp_c in merge_clusters:
         node_count += 1
-        temp_c = paralog_centroid_dict[centroid]
-        G = merge_nodes(G, temp_c.pop(), temp_c.pop(), node_count)
+        G = merge_nodes(G,
+                        temp_c.pop(),
+                        temp_c.pop(),
+                        node_count,
+                        check_merge_mems=False)
         while (len(temp_c) > 0):
-            G = merge_nodes(G, node_count, temp_c.pop(), node_count + 1)
+            G = merge_nodes(G,
+                            node_count,
+                            temp_c.pop(),
+                            node_count + 1,
+                            check_merge_mems=False)
             node_count += 1
 
     return (G)
