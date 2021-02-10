3b4eb9c1a660f7e8593e7c156af496b7ee25806a
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2021-02-10 23:48:46 +1300

    created script to filter presence absence files

---
+++ panaroo-filter-pa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""Wrapper for running the panaroo script to filter the presence/absence matrices"""
+
+from panaroo.filter_pa import main
+
+if __name__ == '__main__':
+    main()
