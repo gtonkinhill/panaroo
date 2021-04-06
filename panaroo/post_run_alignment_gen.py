883ede69182265430054921d611818149d0c1037
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2021-04-07 11:32:00 +1200

    fix for bug in panaroo-msa script that caused it to crash when generating the full set of pangenome alignments.

--- panaroo/post_run_alignment_gen.py
+++ panaroo/post_run_alignment_gen.py
@@ -97,7 +97,7 @@ def main():
         if args.verbose: print("generating pan genome MSAs...")
         generate_pan_genome_alignment(G, temp_dir, args.output_dir, args.n_cpu,
                                       args.alr, isolate_names)
-        core_nodes = get_core_gene_nodes(G, args.core, len(args.input_files))
+        core_nodes = get_core_gene_nodes(G, args.core, len(isolate_names))
         concatenate_core_genome_alignments(core_nodes, args.output_dir)
     elif args.aln == "core":
         if args.verbose: print("generating core genome MSAs...")
