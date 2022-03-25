36ae0a8599d4dd61884a5b44799180aeb53f9664
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2022-03-25 14:05:55 +1100

    added script to extract a fasta file given a gene name

---
+++ panaroo-extract-gene.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""Wrapper for running the panaroo script to extract fasta files for a query gene name"""
+
+from panaroo.extract_gene_fasta import main
+
+if __name__ == '__main__':
+    main()
