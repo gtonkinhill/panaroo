65a8c1fccabe712730d7855a9be6f2889b7e6aca
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2024-02-22 15:57:25 +0100

    minor change to code layout

--- panaroo/generate_alignments.py
+++ panaroo/generate_alignments.py
@@ -143,12 +143,11 @@ def output_dna_and_protein(node, isolate_list, temp_directory, outdir,
     #only output genes with more than one isolate in them
     if isolate_no > 1:
         #set filename to gene name
-        prot_outname = temp_directory + node["name"] + ".fasta"
         if len(prot_outname) >= 237: 
             fastafilename = node["name"][:236]
-            prot_outname = temp_directory + fastafilename + ".fasta"
         else:
             fastafilename = node["name"]
+        prot_outname = temp_directory + fastafilename + ".fasta"
         dna_outname = outdir + "unaligned_dna_sequences/" + fastafilename + ".fasta"
         #Write them to disk time
         SeqIO.write(output_protein, prot_outname, 'fasta')
