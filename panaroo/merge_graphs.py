d5870280a4adf9298dfb1fc2c6fcffd55a6a492b
Neil MacAlasdair <macalasdair@ubu-n-50797e58862a584d.ebi.ac.uk>
2026-06-16 10:05:59 +0100

    Bugfix

--- panaroo/merge_graphs.py
+++ panaroo/merge_graphs.py
@@ -441,7 +441,7 @@ def merge_graphs(directories,
                               alignment="pan",
                               aligner=alr,
                               codons=codons,
-                              codons=strict_codons,
+                              strict_codons=strict_codons,
                               core_threshold=core,
                               subset=None,
                               resume=False)
